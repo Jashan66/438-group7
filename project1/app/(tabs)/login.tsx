082e1378cb5ac2623df48171befe9b4150f79a29
Jashan Rahal <jashanrahal6@gmail.com>
2024-09-05 23:13:50 -0700

    error handling & form validation

--- project1/app/(tabs)/login.tsx
+++ project1/app/(tabs)/login.tsx
@@ -6,6 +6,7 @@ import { initDB, checkLogin } from '../db/db';
 const LoginScreen: React.FC = () => {
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
   useEffect(() => {
     const initializeDatabase = async () => {
@@ -24,17 +25,25 @@ const LoginScreen: React.FC = () => {
     // Example: Navigate to another screen
     // navigation.navigate('HomeScreen');
 
-    loginUser(username, password);
+    if (username === "" || password === "") {
+      setErrorMessage("Username or password cannot be empty!");
+      return;
+    }else{
+      loginUser(username, password);
+    }
+
   };
 
   async function loginUser(username:string, password:string){
     const loggedIn = await checkLogin(username, password);
 
     if(loggedIn){
+      setErrorMessage("");
+      console.log("User logged in");
       
       //navigate to Home
     }else{ 
-      //error
+      setErrorMessage("Incorrect username or password!");
     }
 
   }
@@ -65,6 +74,10 @@ const LoginScreen: React.FC = () => {
       </View>
       
       <Button title="Login" onPress={handleLogin} />
+
+      <View>
+        <Text style={styles.errorMessage}>{errorMessage}</Text>
+      </View>
     </View>
   );
 };
@@ -95,5 +108,13 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingLeft: 10,
     fontSize: 16,
+  },
+  errorMessage: {
+    color:"red",
+    fontSize: 17,
+    textAlign: "center",
+    marginTop: 30
+
+
   },
 });
